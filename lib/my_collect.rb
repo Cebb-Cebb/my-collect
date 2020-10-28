593de86287db07ec6b387911d7c01a15d33ebab0
user <github email address>
2020-10-28 16:03:18 -0400

    commit tree 6f7a9facedeae55017198adaa4bae637c28055c8

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+require 'pry'
+
+def my_collect(collections)
+    i = 0
+    collection = []
+    while i < collections.length
+      collection << yield(collections[i])
+      i += 1
+    end 
+    collection
+  end 
+
 
 
